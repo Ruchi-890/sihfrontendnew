c8a21222ae5f2f0805926918a41cc40cdbbe0af4
sah-lishi <158027503+sah-lishi@users.noreply.github.com>
2024-09-25 12:32:05 +0530

    Delete app/supervisor dashboard/components/Header.js

--- app/supervisor dashboard/components/Header.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-white p-4 shadow-md">
-      <h1 className="text-2xl font-bold">Supervisor Dashboard</h1>
-    </header>
-  );
-}
